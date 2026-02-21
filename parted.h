fb002aaa6b84cbee2ec8cbcf4b555439a205978f
gumoraes525 <gumenezes2019@googlemail.com>
2026-02-21 20:55:55 -0300

    Create header file parted.h with minimal implementation stubs

---
+++ parted.h
@@ -0,0 +1,12 @@
+#ifndef PARTED_H
+#define PARTED_H
+
+// Include necessary headers
+#include <stdio.h>
+
+// Function declarations
+void function1(void);
+int function2(int param);
+void function3(const char* str);
+
+#endif // PARTED_H
